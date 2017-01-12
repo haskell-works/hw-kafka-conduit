0fda6a4dcf16fdfd22a243d5d63b44231d9b3efe
Alexey Raga <alexey.raga@gmail.com>
2017-01-12 20:51:07 +1100

    source works

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # kafka-client-conduit
 
 Conduit based API for `kafka-client`.
+
+## Example
+
+```haskell
+import Control.Monad.IO.Class
+import Conduit
+import Data.Conduit (Source, runConduitRes, (.|))
+import qualified Data.Conduit.List as CL
+import Kafka
+import Kafka.Conduit.Consumer
+
+main :: IO ()
+main = do
+  first5 <- runConduitRes $ creareKafkaStream .| L.take 5
+  print first5
+
+creareKafkaStream :: MonadResource m => Source m (Either KafkaError ReceivedMessage)
+creareKafkaStream = do
+  kc  <- newConsumerConf (ConsumerGroupId "test_group") emptyKafkaProps
+  tc  <- newConsumerTopicConf (TopicProps [("auto.offset.reset", "earliest")])
+  kafkaSource kc tc (BrokersString "localhost:9092") (Timeout 30000) [TopicName "test_topic"]
+```
--- src/Kafka/Conduit/Consumer.hs
+++ src/Kafka/Conduit/Consumer.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE TupleSections#-}
 module Kafka.Conduit.Consumer
 ( kafkaSource
-, kafkaSource'
-, newConsumer
+, newConsumer, closeConsumer
+, pollMessage
 , subscribe
 , newConsumerConf, newConsumerTopicConf
 , ReceivedMessage(..), ConsumerGroupId(..)
+, KafkaError(..), RdKafkaRespErrT(..)
 ) where
 
 import Control.Monad.IO.Class
 import Control.Monad (void)
 import Data.Conduit
 import Kafka
-import Kafka.Consumer (ReceivedMessage, ConsumerGroupId)
+import Kafka.Consumer (ReceivedMessage, ConsumerGroupId, RdKafkaRespErrT(..))
 import qualified Kafka.Consumer as K
 import Control.Monad.Trans.Resource
 
@@ -22,62 +23,82 @@ newConsumerConf :: MonadIO m
                 -> KafkaProps                       -- ^ Extra kafka consumer parameters (see kafka documentation)
                 -> m KafkaConf                      -- ^ Kafka configuration which can be altered before it is used in 'newConsumer'
 newConsumerConf g p = liftIO $ K.newConsumerConf g p
-{-# INLINE newConsumerConf #-}
 
+-- | Creates a new topic configuration from the specified properties
 newConsumerTopicConf :: MonadIO m => TopicProps -> m TopicConf
 newConsumerTopicConf = liftIO . K.newConsumerTopicConf
-{-# INLINE newConsumerTopicConf #-}
 
-newConsumer :: (MonadResource m)
+-- | Creates a new kafka consumer. Note: it must be properly closed after it is not needed.
+newConsumer :: MonadIO m
             => KafkaConf                            -- ^ Consumer config
             -> TopicConf                            -- ^ Topic config that is going to be used for every topic consumed by the consumer
             -> BrokersString                        -- ^ Comma separated list of brokers with ports (e.g. @localhost:9092@)
             -> m Kafka
 newConsumer kc tc bs =
-  let create = K.setDefaultTopicConf kc tc >> K.newConsumer bs kc
-      close  = void <$> K.closeConsumer
-   in snd <$> allocate create close
+  liftIO $ K.setDefaultTopicConf kc tc >> K.newConsumer bs kc
 
+-- | Closes the specified kafka consumer.
+closeConsumer :: MonadIO m => Kafka -> m (Maybe KafkaError)
+closeConsumer = liftIO . K.closeConsumer
+
+-- | Subscribes to a list of topics
 subscribe :: MonadIO m => Kafka -> [TopicName] -> m (Maybe KafkaError)
-subscribe k ts = liftIO $ subscribe k ts
+subscribe k ts = liftIO $ K.subscribe k ts
 {-# INLINE subscribe #-}
 
+-- | Polls one message from the consumer.
 pollMessage :: MonadIO m => Kafka -> Timeout -> m (Either KafkaError ReceivedMessage)
 pollMessage k t = liftIO $ K.pollMessage k t
 {-# INLINE pollMessage #-}
 
+-- | Conduit source for kafka consumer.
 kafkaSource :: MonadResource m
-            => Kafka
-            -> Timeout
-            -> Source m (Either KafkaError ReceivedMessage)
-kafkaSource kafka timeout = go
-  where
-    go = yieldM (pollMessage kafka timeout) >> go
-
-
--- REDO using newConsumer? remove? Don't like these Left/Right cleanup magic
-kafkaSource' :: MonadResource m
             => KafkaConf                            -- ^ Consumer config
             -> TopicConf                            -- ^ Topic config that is going to be used for every topic consumed by the consumer
             -> BrokersString                        -- ^ Comma separated list of brokers with ports (e.g. @localhost:9092@)
+            -> Timeout                              -- ^ Poll timeout
             -> [TopicName]                          -- ^ List of topics to be consumed
-            -> Timeout
             -> Source m (Either KafkaError ReceivedMessage)
-kafkaSource' kc tc bs ts t =
-    bracketP mkConsumer clConsumer runHandler
-    where
-        mkConsumer = do
-            K.setDefaultTopicConf kc tc
-            kafka  <- K.newConsumer bs kc
-            subRes <- K.subscribe kafka ts
-            return $ maybe (Right kafka) (Left . (, kafka)) subRes
+kafkaSource kc tc bs t ts =
+  bracketP mkConsumer clConsumer runHandler
+  where
+      mkConsumer = do
+          kafka  <- newConsumer kc tc bs
+          sres   <- subscribe kafka ts
+          return (kafka, sres)
 
-        clConsumer (Left (_, kafka)) = void $ K.closeConsumer kafka
-        clConsumer (Right kafka) = void $ K.closeConsumer kafka
+      clConsumer (kafka, _) = void $ closeConsumer kafka
 
-        runHandler (Left (err, _)) = return ()
-        runHandler (Right kafka) = do
-          liftIO $ print "Let's poll"
-          msg <- liftIO $ K.pollMessage kafka t
-          yield msg
-          runHandler (Right kafka)
+      runHandler (_, Just err) = void $ yield (Left err)
+      runHandler (kafka, Nothing) = do
+        msg <- pollMessage kafka t
+        -- stop at some certain cases because it is not goind to be better with time
+        case msg of
+          Left err | isFatal err -> void $ yield (Left err)
+          _ -> yield msg >> runHandler (kafka, Nothing)
+
+-- | Checks if the error is fatal in a way that it doesn't make sense to retry.
+isFatal :: KafkaError -> Bool
+isFatal e = case e of
+  KafkaUnknownConfigurationKey _   -> True
+  KafkaInvalidConfigurationValue _ -> True
+  KakfaBadConfiguration            -> True
+  KafkaBadSpecification _          -> True
+
+  -- More of them? Less of them?
+  KafkaResponseError RdKafkaRespErrDestroy                    -> True
+  KafkaResponseError RdKafkaRespErrFail                       -> True
+  KafkaResponseError RdKafkaRespErrInvalidArg                 -> True
+  KafkaResponseError RdKafkaRespErrSsl                        -> True
+  KafkaResponseError RdKafkaRespErrUnknownProtocol            -> True
+  KafkaResponseError RdKafkaRespErrNotImplemented             -> True
+  KafkaResponseError RdKafkaRespErrAuthentication             -> True
+  KafkaResponseError RdKafkaRespErrInconsistentGroupProtocol  -> True
+  KafkaResponseError RdKafkaRespErrTopicAuthorizationFailed   -> True
+  KafkaResponseError RdKafkaRespErrGroupAuthorizationFailed   -> True
+  KafkaResponseError RdKafkaRespErrClusterAuthorizationFailed -> True
+  KafkaResponseError RdKafkaRespErrUnsupportedSaslMechanism   -> True
+  KafkaResponseError RdKafkaRespErrIllegalSaslState           -> True
+  KafkaResponseError RdKafkaRespErrUnsupportedVersion         -> True
+
+  _ -> False

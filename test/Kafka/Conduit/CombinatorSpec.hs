44f346634c289411b42593bbfa5e96d7ce009172
Alexey Raga <alexey.raga@gmail.com>
2017-03-02 21:26:45 +1100

    fix the grid

--- test/Kafka/Conduit/CombinatorSpec.hs
+++ test/Kafka/Conduit/CombinatorSpec.hs
@@ -3,7 +3,7 @@
 module Kafka.Conduit.CombinatorSpec (spec) where
 
 import Data.Conduit
-import Data.Conduit.List as CL
+import qualified Data.Conduit.List as CL
 import Data.List.Extra
 import Kafka.Conduit.Combinator
 import Test.Hspec
@@ -18,7 +18,7 @@ spec = describe "Kafka.Conduit.UtilSpec" $ do
   describe "batchBy" $ do
     it "Should batch properly" $ do
       forAll (choose (1 :: Int, 100)) $ \i -> do
-        xs :: [[Int]] <- runConduit $ sourceList [1..100] .| batchBy i .| CL.consume
+        xs :: [[Int]] <- runConduit $ CL.sourceList [1..100] .| batchBy i .| CL.consume
         P.concat xs `shouldBe` [1..100]
         P.filter (/= i) (length <$> P.init xs) `shouldBe` []
   describe "batchByOrFlush" $ do
@@ -26,7 +26,7 @@ spec = describe "Kafka.Conduit.UtilSpec" $ do
       forAll (choose (1 :: Int, 100)) $ \i ->
         forAll (choose (1 :: Int, 100)) $ \j -> do
           let as = intercalate [Nothing] (chunksOf j (Just <$> [1..100]))
-          xs :: [[Int]] <- runConduit $ sourceList as .| batchByOrFlush i .| CL.consume
+          xs :: [[Int]] <- runConduit $ CL.sourceList as .| batchByOrFlush i .| CL.consume
           P.concat xs `shouldBe` [1..100]
           P.filter (> i) (length <$> P.init xs) `shouldBe` []
           P.filter (> j) (length <$> P.init xs) `shouldBe` []
@@ -34,7 +34,7 @@ spec = describe "Kafka.Conduit.UtilSpec" $ do
       forAll (choose (1 :: Int, 100)) $ \i ->
         forAll (choose (1 :: Int, 100)) $ \j -> do
           let as = intercalate [Nothing, Nothing] (chunksOf j (Just <$> [1..100]))
-          xs :: [[Int]] <- runConduit $ sourceList as .| batchByOrFlush i .| CL.consume
+          xs :: [[Int]] <- runConduit $ CL.sourceList as .| batchByOrFlush i .| CL.consume
           P.concat xs `shouldBe` [1..100]
           P.filter (> i) (length <$> P.init xs) `shouldBe` []
           P.filter (> j) (length <$> P.init xs) `shouldBe` []

6ddba57a299e951dfa5a6de08cb2f9a0c1a2c8e6
Josh Godsiff <jgodsiff@arbor.net>
2017-07-11 11:33:13 +1000

    Added a batchByOrFlush variant for Either.

--- test/Kafka/Conduit/CombinatorSpec.hs
+++ test/Kafka/Conduit/CombinatorSpec.hs
@@ -18,17 +18,26 @@ spec = describe "Kafka.Conduit.UtilSpec" $ do
   describe "batchByOrFlush" $ do
     it "Should batch by i properly when Nothing is interspersed between every j elements" $ do
       forAll (choose (1 :: Int, 100)) $ \i ->
-        forAll (choose (1 :: Int, 100)) $ \j -> do
-          let as = intercalate [Nothing] (chunksOf j (Just <$> [1..100]))
-          xs :: [[Int]] <- runConduit $ CL.sourceList as .| batchByOrFlush (BatchSize i) .| CL.consume
-          P.concat xs `shouldBe` [1..100]
-          P.filter (> i) (length <$> P.init xs) `shouldBe` []
-          P.filter (> j) (length <$> P.init xs) `shouldBe` []
+        forAll (choose (1 :: Int, 100)) $ \j ->
+          testBatchBy batchByOrFlush Just [Nothing] i j
     it "Should batch by i properly when [Nothing, Nothing] is interspersed between every j elements" $ do
       forAll (choose (1 :: Int, 100)) $ \i ->
-        forAll (choose (1 :: Int, 100)) $ \j -> do
-          let as = intercalate [Nothing, Nothing] (chunksOf j (Just <$> [1..100]))
-          xs :: [[Int]] <- runConduit $ CL.sourceList as .| batchByOrFlush (BatchSize i) .| CL.consume
-          P.concat xs `shouldBe` [1..100]
-          P.filter (> i) (length <$> P.init xs) `shouldBe` []
-          P.filter (> j) (length <$> P.init xs) `shouldBe` []
+        forAll (choose (1 :: Int, 100)) $ \j ->
+          testBatchBy batchByOrFlush Just [Nothing, Nothing] i j
+  describe "batchByOrFlushEither" $ do
+    it "Should batch by i properly when Left () is interspersed between every j elements" $ do
+      forAll (choose (1 :: Int, 100)) $ \i ->
+        forAll (choose (1 :: Int, 100)) $ \j ->
+          testBatchBy batchByOrFlushEither Right [Left ()] i j
+    it "Should batch by i properly when [Left (), Left ()] is interspersed between every j elements" $ do
+      forAll (choose (1 :: Int, 100)) $ \i ->
+        forAll (choose (1 :: Int, 100)) $ \j ->
+          testBatchBy batchByOrFlushEither Right [Left (), Left ()] i j
+
+testBatchBy :: (Enum a, Num a) => (BatchSize -> ConduitM b [Int] IO ()) -> (a -> b) -> [b] -> Int -> Int -> IO ()
+testBatchBy batchFn constr inter i j = do
+  let as = intercalate inter (chunksOf j (constr <$> [1..100]))
+  xs :: [[Int]] <- runConduit $ CL.sourceList as .| batchFn (BatchSize i) .| CL.consume
+  P.concat xs `shouldBe` [1..100]
+  P.filter (> i) (length <$> P.init xs) `shouldBe` []
+  P.filter (> j) (length <$> P.init xs) `shouldBe` []
